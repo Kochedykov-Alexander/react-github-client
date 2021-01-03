323d18a2c5b424095edaf12c7e2e814668230847
AVKochedykov <AVKochedykov@stud.kpfu.ru>
2021-01-03 21:54:22 +0500

    add auth part1

--- react-github-client/src/App.tsx
+++ react-github-client/src/App.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { createContext, useReducer } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import { initialState, reducer } from "./store/reducer";
 
+
+export const AuthContext = React.createContext(initialState);
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <div className="App">
-      hello react
-    </div>
+    <AuthContext.Provider
+      value={
+        {
+        state,
+        dispatch
+      }
+    }
+    >
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login}/>
+        <Route path="/" component={Home}/>
+      </Switch>
+    </Router>
+    </AuthContext.Provider>
   );
 }
 
